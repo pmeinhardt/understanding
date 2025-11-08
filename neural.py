89d50f5ca3d111a9974fd6d675a2a86be46522b6
pmeinhardt <code@mnhrdt.io>
2025-11-08 23:02:13 +0100

    Allow specifying different activation functions

--- neural.py
+++ neural.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from itertools import pairwise
+from typing import Callable
 
 import graphlib
 import math
@@ -132,21 +133,22 @@ def backprop(start: Node):
 
 
 class Neuron:
-  def __init__(self, nin: int):
+  def __init__(self, nin: int, phi: Callable[[Node], Node]):
     self.w = [Value(random.uniform(-1.0, 1.0)) for _ in range(nin)] # weights
     self.b = Value(random.uniform(-1.0, 1.0)) # bias
+    self.f = phi # activation function
 
   def __call__(self, x: list[Node | float]):
     act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
-    return tanh(act)
+    return self.f(act)
 
   def params(self):
     return self.w + [self.b]
 
 
 class Layer:
-  def __init__(self, nin: int, nout: int):
-    self.neurons = [Neuron(nin) for _ in range(nout)]
+  def __init__(self, nin: int, nout: int, phi: Callable[[Node], Node]):
+    self.neurons = [Neuron(nin, phi) for _ in range(nout)]
 
   def __call__(self, x: list[Node | float]):
     return [n(x) for n in self.neurons]
@@ -165,8 +167,8 @@ class Layer:
 
 
 class MLP:
-  def __init__(self, nin: int, nouts: list[int]):
-    self.layers = [Layer(nin, nout) for nin, nout in pairwise([nin] + nouts)]
+  def __init__(self, nin: int, nouts: list[int], phi: Callable[[Node], Node]):
+    self.layers = [Layer(nin, nout, phi) for nin, nout in pairwise([nin] + nouts)]
 
   def __call__(self, x: list[Node | float]):
     x = [xi if isinstance(xi, Node) else Value(xi) for xi in x]
@@ -186,7 +188,7 @@ class MLP:
 
 
 if __name__ == '__main__':
-  nn = MLP(3, [4, 4, 1]) # 3 inputs, 2 hidden layers of 4 neurons, 1 output
+  nn = MLP(3, [4, 4, 1], tanh) # 3 inputs, 2 hidden layers of 4 neurons, 1 output
 
   params = nn.params() # gather parameters
 
